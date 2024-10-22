4fc1186f86fd7e4be640bed87904702bd7547ad4
Bharat Chauhan <bharatchauhan.us@protonmail.com>
2024-10-21 21:07:16 -0400

    Make date picker responsive for mobile devices

--- app/components/date-picker..tsx
+++ app/components/date-picker..tsx
@@ -25,12 +25,14 @@ export function DatePicker({ date, setDate }: DatePickerProps) {
         <Button
           variant={"outline"}
           className={cn(
-            "w-[280px] justify-start text-left font-normal",
+            "w-12 md:w-[280px] justify-start text-left font-normal",
             !date && "text-muted-foreground"
           )}
         >
           <CalendarIcon className="mr-2 h-4 w-4" />
-          {date ? format(date, "PPP") : <span>Pick a date</span>}
+          <span className="hidden md:block">
+            {date ? format(date, "PPP") : <span>Pick a date</span>}
+          </span>
         </Button>
       </PopoverTrigger>
       <PopoverContent className="w-auto p-0">
